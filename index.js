f5e52bc0c37dcdad972bcfe6befe5859dda6b058
Duongle2002 <duong100008@donga.edu.vn>
2024-11-09 16:04:49 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,8 +1,44 @@
-let promise = new Promise((resoluve , reject)=>{
-    resoluve("Hi resoluve ")
-})
-promise.then((data)=>{
+function getUserId(){
+    setTimeout(() => {
+        let UserId = 10
+    }, 0);
+    fi(Us)
+}
+
+function getPostId(UserId){
+    setTimeout(() => {
+        return PostId = 111
+    }, 0);
+}
+function getComnent(PostId){
+    setTimeout(() => {
+        return "sjdhjkdh"
+    }, 0);t
+}
+console.log(getUserId())
+// let promise = new Promise((resolve , reject)=>{
+//     resolve("Hi resoluve ")
+// })
+
+// promise.then((data)=>{
+//     console.log(data);
+// }).catch((err)=>{
+//     console.log(err)
+
+
+
+async function abc() {
+    let UserId = await getUserId();
+    let PostId = await getPostId(UserId);
+    let Comment = await getComnent(PostId);
+    return Comment;
+}
+
+abc().then((data)=>{
     console.log(data);
-}).catch((err)=>{
-    console.log(err)
+});
+
+Promise.all([getUserId(), getPostId(), getComnent()].then
+    (value)=>{
+    console.log("all",value);
 })
