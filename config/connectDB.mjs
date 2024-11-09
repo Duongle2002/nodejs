b39d665389a190a53749fdd657b453b2ec5b6788
Duongle2002 <duong100008@donga.edu.vn>
2024-11-09 16:09:13 +0700

    Add files via upload

---
+++ config/connectDB.mjs
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+
+export function connectDB() {
+  mongoose
+    .connect("mongodb+srv://huuduongle2002:Duongmb8602@cluster0.andq2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    .then(() => console.log("Connected!"));
+}
--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-// let a = 10;
-// let b = 20; 
-// let c = a + b ;
-// console.log("C = ", c);
-import http from 'node:http';
+import express from "express";
+import rootRouter from "./routes/root.mjs";
+import { connectDB } from "./config/connectDB.mjs";
+connectDB();
+const app = express();
+const port = 3000;
 
-http 
+app.set("view engine", "ejs");
+app.set("views", "./views");
+app.use(express.static("public"));
+
+app.use("/", rootRouter);
+
+app.listen(port, () => {
+  console.log("Server stated!!!");
+});
